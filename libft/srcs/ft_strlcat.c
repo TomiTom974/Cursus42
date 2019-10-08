533b7b829a830cede69f7081808ac5d673df1820
Tom sur pc Alicia <mrtomico78@gmail.com>
2019-10-08 15:24:12 +0200

    les strL

---
+++ libft/srcs/ft_strlcat.c
@@ -0,0 +1,31 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tobarite <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/10/08 14:44:09 by tobarite          #+#    #+#              #
+#                                                     ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+int		ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	int				i;
+	unsigned int	dest_size;
+
+	i = 0;
+	dest_size = 0;
+	while (dest[dest_size])
+		dest_size++;
+	while (src[i])
+	{
+		if (dest_size < size - 1)
+			dest[dest_size] = src[i];
+		dest_size++;
+		i++;
+	}
+	dest[dest_size - 1] = '\0';
+	return (dest_size);
+}
