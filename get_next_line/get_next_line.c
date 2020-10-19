b9c301c2795904495dfedd7687d98f17830d187c
Tom BARITEAU PETER <tobarite@e1r4p14.42.fr>
2020-10-19 18:48:44 +0200

    avancement de GNL 19/10/2020

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,70 +6,113 @@
 /*   By: tobarite <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 13:16:29 by tobarite          #+#    #+#             */
-/*   Updated: 2020/10/19 14:45:20 by tobarite         ###   ########.fr       */
+/*   Updated: 2020/10/19 18:46:59 by tobarite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//int           get_next_line(const int fd, char **line)
-//{
-//      si une erreur
-//              renvoyer -1
-//      lire sur le fd de la taille du buffer
-//              si \n
-//                      mettre a l'adresse *line jusqu'au \n
-//                      stocker dans la static la suite de \n (free avant la ou on stock)
-//                      renvoyer 1
-//              si \0
-//                      mettre la static dans line
-//                      free la static
-//                      renvoyer 0
-//
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
 
 int		ft_check_buf(char *buf)
 {
+	printf("\nCHECK BUFF\n");
 	int i;
 
 	i = 0;
 	while (buf[i])
 	{
 		if (buf[i] == '\n')
+		{
+			printf("\nON A BIEN FINI Y'A UN BACKSLASH N\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
 }
 
-int		get_next_line(const int fd, char **line)
+int		ft_strncmp(const char *s1, const char *s2, int n)
 {
-	char		buf[BUFF_SIZE + 1];
-	static char	*stat;
-	int			i;
+	int i;
 
 	i = 0;
-	if (fd < 0 || BUFF_SIZE < 1 ||line == NULL || read(fd, buf, 0))
-                return (-1);
-	while (ft_check_buf(buf))
-	{
-		read(fd, &buf, BUFF_SIZE);
-		i = 0;
-		while (buf[i])
-		{
-			*stat = buf[i];
-			stat++;
-			i++;
-		}
-	}
-				return (1);
+	while (s1[i] && s2[i] && i != n)
+		i++;
+	return (s1[i] - s2[i]);
 }
 
-//int		main(void)
-//{
-//	char *ligne;
-//
-//	ligne = "\0";
-//	get_next_line(0, &ligne);
-//	printf("%s", ligne);
-//	return (0);
-//}
+void	ft_task(char *stat, char **line)
+{
+	int		i;
+	char	c;
+
+	c = '\n';
+	i = 0;
+	printf("%s\n", stat);
+	printf("J'ENTRE DANS TASK\n");
+	
+	while (stat[i] != '\n' && stat[i] != '\0')
+	{
+		printf("ON COMMENCRE LE PARSING DE BACKSLASH N\n");
+		**line = stat[i];
+		line++;
+		i++;
+	}
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	char		buf[BUFF_SIZE];
+	static char	*stat;
+	int			i;
+	int			y;
+
+	stat = malloc(sizeof(char *));
+	i = 0;
+	y = ft_strlen(stat);
+	if (fd < 0 || BUFF_SIZE < 1 || line == NULL || read(fd, buf, 0))
+		return (-1);
+	while (ft_check_buf(stat))
+	{
+		read(fd, buf, BUFF_SIZE);
+		printf("---------------------------------\n");
+		printf("mon buf apres le read principal : \n%s\n", buf);
+		printf("---------------------------------\n");
+		i = 0;
+		while (i < BUFF_SIZE)
+		{
+			stat[y] = buf[i];
+			y++;
+			i++;
+		}
+		printf("---------------------------------\n");
+		printf("ma static apres le read principal : \n%s\n", stat);
+		printf("---------------------------------\n");
+	}
+	printf("ATTENTION TASK J'ARRIVE\n");
+	ft_task(stat, line);
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	char *line;
+	int fd = open(av[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,18 +6,19 @@
 /*   By: tobarite <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 13:31:09 by tobarite          #+#    #+#             */
-/*   Updated: 2020/10/19 14:44:40 by tobarite         ###   ########.fr       */
+/*   Updated: 2020/10/19 18:47:01 by tobarite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#ifndef BUFF_SIZE
-# define BUFF_SIZE
-#endif
 # define GET_NEXT_LINE_H
+# ifndef BUFF_SIZE
+#  define BUFF_SIZE
+# endif
 # include "stdlib.h"
 # include "unistd.h"
 # include "stdio.h"
+# include "libc.h"
 
 int		get_next_line(const int fd, char **line);
 
