ce31510d709921466514cb819e88c9fc2c778a05
Tom BARITEAU PETER <tobarite@e1r4p8.42.fr>
2021-02-19 16:50:32 +0100

    avancement de GNL 19/02/2021

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tobarite <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 13:16:29 by tobarite          #+#    #+#             */
-/*   Updated: 2021/02/19 15:58:46 by tobarite         ###   ########.fr       */
+/*   Updated: 2021/02/19 16:45:20 by tobarite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,22 @@ char	*ft_substr(char *s, unsigned int start, int len)
 	int		size;
 	char	*tab;
 
-	printf("HHH\n");
 	count = 0;
-	printf("III\n");
 	//if (!s)
 	//	return (NULL);
-	printf("JJJ\n");
 	if (ft_strlen(s) < (int)start)
 		return (ft_strdup(""));
-	printf("KKK\n");
 	size = ft_strlen(s + start);
-	printf("LLL\n");
 	if (size < len)
 		len = size;
-	printf("MMM\n");
 	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
 		return (NULL);
-	printf("NNN\n");
 	while (count < len)
 	{
 		tab[count] = s[start + count];
 		count++;
 	}
-	printf("OOO\n");
 	tab[count] = '\0';
-	printf("PPP\n");
 	return (tab);
 }
 
@@ -67,7 +58,7 @@ int		ft_strlen_n(char *str)
 	int i;
 
 	i = 0;
-	if (!str)
+	if (str == NULL)
 		return (0);
 	while (str[i] != '\n')
 		i++;
@@ -95,38 +86,20 @@ int		get_next_line(const int fd, char **line)
 	int			v;
 	char		*tmp;
 
-	printf("AAA\n");
 	stoc = NULL;
 	tmp = NULL;
 	v = 1;
-	printf("BBB\n");
 
 	//if (fd < 0 || BUFF_SIZE < 1 || line == NULL)
 	//	return (-1);
-	while (v == 1)
+	while (ft_check(buf)== 1)
 	{
 		if (!stoc)
 			stoc = ft_malloc_it(1);
-		printf("CCC\n");
-		v = ft_check(buf);
-		printf("DDD\n");
 		read(fd, buf, BUFF_SIZE);
 		printf("\nbuf ressemble a ca : %s\n\n", buf);
-		printf("EEE\n");
-		//if (v == 1 || v == 2)
-		//{
-		//	printf("\nON STOC UN COUP\n\n");
 		stoc = ft_strjoin(stoc, buf);
-		//}
-		//printf("FFF\n");
-		//if (v == 0)
-		//{
-		//	printf("\nON STOC UN COUP\n\n");
-		//	stoc = ft_strjoin(stoc, ft_substr(buf, 0, ft_strlen_n(buf)));
-		//}
-		printf("GGG\n");
 		printf("\nStoc ressemble a ca : %s\n\n", stoc);
-		printf("QQQ\n");
 	}
 	*line = ft_substr(stoc, 0, ft_strlen_n(stoc));
 	if (v == 0)
